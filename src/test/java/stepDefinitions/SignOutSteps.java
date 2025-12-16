03d1e1cabf63ac862847a4d570c934a0fdd75e77
elavarasi2888 <elavarasi2888@gmail.com>
2025-12-15 23:21:22 -0500

    added signout feature file and step defn

---
+++ src/test/java/stepDefinitions/SignOutSteps.java
@@ -0,0 +1,26 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class SignOutSteps {
+	@Given("User is in the Home page after Sign in")
+	public void User_is_in_the_Home_page_after_Sign_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@When("User clicks \"Sign out\"")
+	public void User_clicks_Sign_Out()
+	{
+		// Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("User should be redirected to home page with message \"Logged out successfully\"")
+	public void user_should_be_redirected_to_home_page_with_message_Logged_out_successfully(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+			
+}
---
+++ src/test/resources/features/signOut.feature
@@ -0,0 +1,9 @@
+Feature: Sign out functionality
+# Functional test case
+
+  Background:
+    Given User is in the Home page after Sign in
+
+  Scenario: Verify that user able to sign out successfully
+    When User clicks "Sign out"
+    Then User should be redirected to home page with message "Logged out successfully"
