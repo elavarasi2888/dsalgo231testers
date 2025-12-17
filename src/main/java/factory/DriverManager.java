8847f772d0d7fb078267200acc75f27c2edfd671
showri <showrijb@gmail.com>
2025-12-16 22:23:31 -0500

    Added Driver factory, Hooks, test runner, Browser options, config reader, log4j2.xml,logger factory, tree page

--- src/main/java/factory/DriverManager.java
+++ src/main/java/factory/DriverManager.java
@@ -1,8 +1,40 @@
 package factory;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import utils.BrowserOptions;
 
 public class DriverManager {
 
+	private static BrowserOptions browserOptions = new BrowserOptions();
+	private static final ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
 
-   
+	public static ChromeOptions co = browserOptions.chromeOption();
+	public static EdgeOptions ed = browserOptions.edgeOption();
+	public static FirefoxOptions fo = browserOptions.firefoxOption();
+
+	public static WebDriver initBrowser(String browser) {
+
+		LoggerFactory.getLogger().info("In launchBrowser() - browser value-{}", browser);
+		if (browser.equalsIgnoreCase("chrome")) {
+			driver.set(new ChromeDriver(co));
+		} else if (browser.equalsIgnoreCase("edge")) {
+			driver.set(new EdgeDriver(ed));
+		} else if (browser.equalsIgnoreCase("firefox")) {
+			driver.set(new FirefoxDriver(fo));
+		}
+
+		return getDriver();
+	}
+
+	public static WebDriver getDriver() {
+		return driver.get();
+	}
+
 }
--- src/test/java/testRunner/runner.java
+++ src/test/java/testRunner/runner.java
@@ -1,7 +1,27 @@
 package testRunner;
 
+import org.testng.annotations.DataProvider;
 
-        
-public class runner {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features = "src/test/resources/features", glue = { "hooks",
+		"stepDefinitions" }, tags = "    ", plugin = { "pretty",
+				"html:reports/cucumber-reports.html" }, dryRun = false, monochrome = true)
+public class runner extends AbstractTestNGCucumberTests {
+
+	@Override
+	@DataProvider(parallel = false)
+	public Object[][] scenarios() {
+		return super.scenarios();
+	}
+
+	/*
+	 * @BeforeClass
+	 * 
+	 * @Parameters({"browsertype"})//coming from testNG.xml public void
+	 * beforeClass(@Optional String browser) throws IOException {
+	 * //ConfigReader.browserType=browser; ConfigReader.setBrowserType(browser); }
+	 */
 
 }
