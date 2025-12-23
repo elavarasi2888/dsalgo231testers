450fd4d15a08aa8fdb30b08c268dbcd0bb79d742
elavarasi2888 <elavarasi2888@gmail.com>
2025-12-23 00:15:08 -0500

    signin modified a little

--- src/test/java/stepDefinitions/StackSteps.java
+++ src/test/java/stepDefinitions/StackSteps.java
@@ -9,20 +9,21 @@ import pageObjects.StackPage;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
-public class StackSteps 
-{
+public class StackSteps {
 	StackPage stackPage = new StackPage(DriverManager.getDriver());
-	 // ---------------- Background ----------------
-	 
+
+	// ---------------- Background ----------------
+
 	@Given("User clicks the Getting Started button in {string} Panel")
-	    public void user_clicks_getting_started_button(String panelName) {
-		//homePage.clickGettingStarted(panelName);
-	    }
+	public void user_clicks_getting_started_button(String panelName) {
+		// homePage.clickGettingStarted(panelName);
+	}
+
 	@Given("User is on Stack page")
 	public void user_is_on_stack_page(String panelName) {
-		assertTrue(stackPage.isOnStackPage(),"User is not on Stack page");
+		assertTrue(stackPage.isOnStackPage(), "User is not on Stack page");
 	}
-	 // ---------------- Non Functional -------------------------------------
+	// ---------------- Non Functional -------------------------------------
 
 	@Then("user should see {string} header for Stack page")
 	public void user_should_see_header_for_stack_page(String expectedHeader) {
@@ -30,66 +31,53 @@ public class StackSteps
 	}
 
 	@Then("user should see {string} link of Stack page")
-	public void user_should_see_link_of_stack_page(String topicName) 
-	{
-		switch (topicName) 
-		{
+	public void user_should_see_link_of_stack_page(String topicName) {
+		switch (topicName) {
 		case "Operations in Stack":
-			assertTrue(stackPage.isOperationsInStackDisplayed(),
-                    topicName + " link is NOT visible on Stack page");
-            break;
-        case "Implementation":
-        	assertTrue(stackPage.isImplementationDisplayed(),
-                    topicName + " link is NOT visible on Stack page");
-            break;
-        case "Applications":
-        	assertTrue(stackPage.IsApplicationsDisplayed(),
-                    topicName + " link is NOT visible on Stack page");
-            break;
-        default:
-            throw new RuntimeException("Topic not found: " + topicName);
+			assertTrue(stackPage.isOperationsInStackDisplayed(), topicName + " link is NOT visible on Stack page");
+			break;
+		case "Implementation":
+			assertTrue(stackPage.isImplementationDisplayed(), topicName + " link is NOT visible on Stack page");
+			break;
+		case "Applications":
+			assertTrue(stackPage.IsApplicationsDisplayed(), topicName + " link is NOT visible on Stack page");
+			break;
+		default:
+			throw new RuntimeException("Topic not found: " + topicName);
 		}
 	}
+
 	@Then("user should see {string} header of the respective Stack page")
 	public void user_should_see_header_of_the_respective_stack_page(String header) {
-		 assertEquals(stackPage.getTopicsCoveredHeader(), header);
+		assertEquals(stackPage.getTopicsCoveredHeader(), header);
 	}
 
 	@Then("User should see {string} button on the Stack page")
 	public void user_should_see_try_here_button_on_the_stack_page() {
-		assertTrue(stackPage.isTryHereVisible(),
-                "Try Here button not visible");
+		assertTrue(stackPage.isTryHereVisible(), "Try Here button not visible");
 	}
 
-	
-	  // ---------------- Functional ---------------------------------------	
-	
+	// ---------------- Functional ---------------------------------------
 
 	@When("user clicks {string} link on the Stack page")
-	public void user_clicks_link_on_the_stack_page(String topicName) 
-	{
-		 switch (topicName) {
-	        case "Operations in Stack":
-	        	stackPage.clickOperations();
-	            break;
-	        case "Implementation":
-	        	stackPage.clickImplementation();
-	            break;
-	        case "Applications":
-	        	stackPage.clickApplications();
-	            break;
-	        default:
-	            throw new RuntimeException("Topic not found: " + topicName);
-	    }
+	public void user_clicks_link_on_the_stack_page(String topicName) {
+		switch (topicName) {
+		case "Operations in Stack":
+			stackPage.clickOperations();
+			break;
+		case "Implementation":
+			stackPage.clickImplementation();
+			break;
+		case "Applications":
+			stackPage.clickApplications();
+			break;
+		default:
+			throw new RuntimeException("Topic not found: " + topicName);
+		}
 	}
 
-	
 	@Then("user should be directed to {string} page of Stack page")
 	public void user_should_be_directed_to_page_of_stack_page(String expectedPage) {
-		 assertTrue(stackPage.isUrlContains(expectedPage),
-	                "URL does not contain expected text");
+		assertTrue(stackPage.isUrlContains(expectedPage), "URL does not contain expected text");
 	}
 }
-
-
-
