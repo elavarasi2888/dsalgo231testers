72cbf64c449f0b0a9103c28795f5e7a4b328cfcf
showri <showrijb@gmail.com>
2025-12-23 20:53:02 -0500

    Updated QueuePage and TreePage

--- src/main/java/pageObjects/TreePage.java
+++ src/main/java/pageObjects/TreePage.java
@@ -1,14 +1,80 @@
 package pageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TreePage {
-	private WebDriver driver;
+    private WebDriver driver;
 
-	public TreePage(WebDriver driver) {
+    By headerTree = By.xpath("//h4[normalize-space()='Tree']");
+    By headerTopicsCoveredTree = By.xpath("//p[@class='bg-secondary text-white']");
+    By lnkTreeLinks = By.xpath("//a[@class='list-group-item']");
+    By headerTreeLink = By.xpath("//div[@class='col-sm']//strong//p");
+    By btnTryHereTreeLinkPage = By.xpath("//a[normalize-space()='Try here>>>']");
 
-		this.driver = driver;
-	}
+    public TreePage(WebDriver driver) {
+
+        this.driver = driver;
+    }
+
+    public boolean isTreeHeaderVisible() {
+
+        return driver.findElement(headerTree).isDisplayed();
+    }
+
+    public boolean isTopicsCoveredHeaderTreeVisible() {
+
+        return driver.findElement(headerTopicsCoveredTree).isDisplayed();
+    }
+
+    public List<String> treeLinksVisible() {
+
+        List<String> a = new ArrayList<>();
+        List<WebElement> treeLinks = driver.findElements(lnkTreeLinks);
+        for (WebElement t : treeLinks) {
+            a.add(t.getText().toLowerCase());
+        }
+        return a;
+    }
+
+
+    public boolean isTreeLinksVisible(String treeTopicLink) {
+        List<WebElement> treeLinks = driver.findElements(lnkTreeLinks);
+        for (WebElement t : treeLinks) {
+            if (t.getText().equals(treeTopicLink))
+                return true;
+        }
+
+        return false;
+    }
+
+    public void clickTreeTopicLink(String treeTopicLink) {
+        By linkPath = By.xpath("//a[text() = '" + treeTopicLink + "']");
+        driver.findElement(linkPath).click();
+    }
+
+    public String getTreePageURL() {
+        return driver.getCurrentUrl();
+    }
+
+    public String getTreeLinkHeader() {
+
+        return driver.findElement(headerTreeLink).getText();
+    }
+
+    public boolean isTryHereButtonOnTreeLinkPageVisible() {
+
+        return driver.findElement(btnTryHereTreeLinkPage).isDisplayed();
+    }
+
+    public void clickTryHereInTreeLinkPage() {
+        driver.findElement(btnTryHereTreeLinkPage).click();
+
+    }
 
 
 }
--- src/test/java/stepDefinitions/TreeStepDefinitions.java
+++ src/test/java/stepDefinitions/TreeStepDefinitions.java
@@ -1,49 +1,82 @@
 package stepDefinitions;
 
+import factory.DriverManager;
+import factory.LoggerFactory;
+import io.cucumber.java.PendingException;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import pageObjects.HomePage;
+import pageObjects.TreePage;
 
 public class TreeStepDefinitions {
 
-	@Given("User clicks the Getting Started button in Tree Panel")
-	public void user_clicks_the_getting_started_button_in_tree_panel() {
+    WebDriver driver;
+    HomePage homePage;
+    TreePage treePage;
 
-	}
+    public TreeStepDefinitions() {
+        driver = DriverManager.getDriver();
+        homePage = new HomePage(driver);
+        treePage = new TreePage(driver);
+    }
 
-	@Given("User is on the Tree page")
-	public void user_is_on_the_tree_page() {
+    @Given("User clicks the Get Started button from {string} Panel")
+    public void userClicksTheGetStartedButtonFromPanel(String panelName) {
+        treePage = (TreePage) homePage.clickGetStartedButtonOfGivenDsType(panelName);
+    }
 
-	}
+    @Given("User is on the Tree page")
+    public void user_is_on_the_tree_page() {
+        LoggerFactory.getLogger().info("Currently user is at {}",
+                driver.getCurrentUrl());
+    }
 
-	@Then("User should see {string} header for Tree page")
-	public void user_should_see_header_for_tree_page(String string) {
+    @Then("User should see {string} header for Tree page")
+    public void user_should_see_header_for_tree_page(String header) {
+        if (header.equalsIgnoreCase("tree"))
+            Assert.assertTrue(treePage.isTreeHeaderVisible());
 
-	}
+        if (header.equalsIgnoreCase("Topics Covered"))
+            Assert.assertTrue(treePage.isTopicsCoveredHeaderTreeVisible());
 
-	@When("User clicks {string} link on the Tree page")
-	public void user_clicks_link_on_the_tree_page(String string) {
+    }
 
-	}
+    @Then("User should see {string} link of Tree page")
+    public void userShouldSeeLinkOfTreePage(String string) {
+        Assert.assertTrue(treePage.isTreeLinksVisible(string));
+    }
 
-	@Then("User should see {string} header of the respective Tree page")
-	public void user_should_see_header_of_the_respective_tree_page(String string) {
+    @When("User clicks {string} link on the Tree page")
+    public void user_clicks_link_on_the_tree_page(String string) {
+        treePage.clickTreeTopicLink(string);
 
-	}
+    }
 
-	@Then("User should see try here button on the Tree page")
-	public void user_should_see_try_here_button_on_the_tree_page() {
+    @Then("User should see {string} header of the respective Tree page")
+    public void user_should_see_header_of_the_respective_tree_page(String expectedTreeLinkHeader) {
+        String actualTreeLinkHeader = treePage.getTreeLinkHeader();
+        Assert.assertEquals(actualTreeLinkHeader, expectedTreeLinkHeader);
+    }
 
-	}
+    @Then("User should see try here button on the Tree page")
+    public void user_should_see_try_here_button_on_the_tree_page() {
+        Assert.assertTrue(treePage.isTryHereButtonOnTreeLinkPageVisible());
+    }
 
-	@Then("User should be directed to {string} of Tree data structure")
-	public void user_should_be_directed_to_of_tree_data_structure(String string) {
+    @Then("User should be directed to {string} of Tree data structure")
+    public void user_should_be_directed_to_of_tree_data_structure(String treeTopicPage) {
+        String currentURL = driver.getCurrentUrl();
+        Assert.assertTrue(currentURL.contains(treeTopicPage));
 
-	}
+    }
 
-	@When("User clicks Try Here button in the repsective Tree page")
-	public void user_clicks_try_here_button_in_the_repsective_tree_page() {
+    @When("User clicks Try Here button in the repsective Tree page")
+    public void user_clicks_try_here_button_in_the_repsective_tree_page() {
+        treePage.clickTryHereInTreeLinkPage();
 
-	}
+    }
 
 }
