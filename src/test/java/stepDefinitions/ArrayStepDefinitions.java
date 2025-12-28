57a75d992786be35b8ad845b2ce8b6f24cae902b
elavarasi2888 <elavarasi2888@gmail.com>
2025-12-28 09:47:06 -0500

    array without practice and stack working files are committed

--- src/test/java/stepDefinitions/ArrayStepDefinitions.java
+++ src/test/java/stepDefinitions/ArrayStepDefinitions.java
@@ -1,223 +1,76 @@
 package stepDefinitions;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
-
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 
 import factory.DriverManager;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import pageObjects.ArrayPage;
-import pageObjects.ArrayPracticePage;
 import pageObjects.HomePage;
 
 public class ArrayStepDefinitions {
-	 WebDriver driver;
-     ArrayPage arrayPage;
-     ArrayPracticePage pqPage;
-     HomePage homePage;
+    WebDriver driver;
+    ArrayPage arrayPage;
+    HomePage homePage;
 
-     public ArrayStepDefinitions() {
-         driver = DriverManager.getDriver();
-         arrayPage = new ArrayPage(driver);
-         pqPage = new ArrayPracticePage(driver);
-         homePage = new HomePage(driver);
-     }
+    public ArrayStepDefinitions() {
+        driver = DriverManager.getDriver();
+        arrayPage = new ArrayPage(driver);
+        homePage = new HomePage(driver);
+    }
 
-     // ======================= Background / Navigation =======================
+    @Given("User clicks Get Started button from Array panel")
+    public void user_clicks_get_started_button_from_array_panel() {
+        arrayPage = (ArrayPage) homePage.clickGetStartedButtonOfGivenDsType("Array");
+    }
 
-     @Given("User clicks Get Started button from {string} panel")
-     public void user_clicks_get_started_button_from_panel(String panelName) {
-         arrayPage = (ArrayPage) homePage.clickGetStartedButtonOfGivenDsType(panelName);
-     }
+    @Then("User should see Array header for Array page")
+    public void user_should_see_array_header_for_array_page() {
+        Assert.assertTrue(arrayPage.isArrayHeaderVisible());
+    }
 
-     @Given("User is on Array {string} page")
-     public void user_is_on_Array_page(String pageName) {
+    @Then("User should see Topics Covered header for Array page")
+    public void user_should_see_topics_covered_header_for_array_page() {
+        Assert.assertTrue(arrayPage.isTopicsCoveredHeaderForArrayVisible());
+    }
 
-         switch (pageName.toLowerCase()) {
+    @Then("User should see {string} link of Array page")
+    public void user_should_see_link_of_array_page(String string) {
+        Assert.assertTrue(arrayPage.isArrayLinkVisible(string));
+    }
 
-             case "array":
-                 arrayPage = new ArrayPage(driver);
-                 assertTrue(arrayPage.isOnArrayPage(), "User is NOT on Array page");
-                 break;
+    @When("User clicks {string} link on the Array page")
+    public void user_clicks_link_on_the_array_page(String string) {
+        arrayPage.clickArrayTopicLink(string);
+    }
 
-             case "practice questions":
-                 pqPage = new ArrayPracticePage(driver);
-                 assertTrue(pqPage.isOnPracticeQuestionsPage(),
-                         "User is NOT on Practice Questions page");
-                 break;
+    @Then("User should see {string} header of the respective Array page")
+    public void user_should_see_header_of_the_respective_array_page(String expectedArrayLinkHeader) {
+        String actualArrayLinkHeader = arrayPage.getArrayLinksTopicHeader();
+        Assert.assertEquals(actualArrayLinkHeader, expectedArrayLinkHeader);
+    }
 
-             default:
-                 throw new RuntimeException("Unknown page: " + pageName);
-         }
-     }
+    @Then("User should see try here button on the Array page")
+    public void user_should_see_try_here_button_on_the_array_page() {
+        Assert.assertTrue(arrayPage.isTryHereButtonVisible());
+    }
 
-     // ======================= Array Page – Non-Functional =======================
+    @Then("User should be directed to {string} page of Array page")
+    public void user_should_be_directed_to_page_of_array_page(String arrayTopicPage) {
+        String currentURL = driver.getCurrentUrl();
+        Assert.assertTrue(currentURL.contains(arrayTopicPage));
+    }
 
-     @Then("User should see {string} header for Array page")
-     public void user_should_see_header_for_array_page(String expectedHeader) {
-         assertTrue(arrayPage.isHeaderVisible(), "Array page header is NOT visible");
-         assertEquals(arrayPage.getHeaderText(), expectedHeader);
-     }
-
-     @Then("User should see Topics Covered header")
-     public void user_should_see_topics_covered_header(String expectedHeader) {
-         assertTrue(arrayPage.isTopicsCoveredHeaderVisible(),
-                 "Topics Covered header is NOT visible");
-         assertEquals(arrayPage.getTopicsCoveredHeaderText(), expectedHeader);
-     }
-
-     @Then("User should see {string} link of Array page")
-     public void user_should_see_link_of_array_page(String topicName) {
-
-         switch (topicName) {
-             case "Arrays in Python":
-                 assertTrue(arrayPage.isArraysInPythonDisplayed());
-                 break;
-             case "Arrays Using List":
-                 assertTrue(arrayPage.isArraysUsingListDisplayed());
-                 break;
-             case "Basic Operations in Lists":
-                 assertTrue(arrayPage.isBasicOperationsInListDisplayed());
-                 break;
-             case "Applications of Array":
-                 assertTrue(arrayPage.isApplicationsOfArrayDisplayed());
-                 break;
-             default:
-                 throw new RuntimeException("Topic not found: " + topicName);
-         }
-     }
-
-     @Then("User should see {string} button on the Array page")
-     public void user_should_see_button_on_the_array_page(String buttonText) {
-         assertTrue(arrayPage.isTryHereButtonVisible(),
-                 "Try Here button is NOT visible");
-     }
-
-     // ======================= Array Page – Functional =======================
-
-     @When("User clicks {string} link on the Array page")
-     public void user_clicks_link_on_array_page(String topicName) {
-
-         switch (topicName) {
-             case "Arrays in Python":
-                 arrayPage.clickArraysInPython();
-                 break;
-             case "Arrays Using List":
-                 arrayPage.clickArraysUsingList();
-                 break;
-             case "Basic Operations in Lists":
-                 arrayPage.clickBasicOperationsInList();
-                 break;
-             case "Applications of Array":
-                 arrayPage.clickApplicationsOfArray();
-                 break;
-             default:
-                 throw new RuntimeException("Topic not found: " + topicName);
-         }
-     }
-
-     @When("User clicks {string} button on the Array page")
-     public void user_clicks_try_here_button(String buttonText) {
-         arrayPage.clickTryHereButton();
-     }
-
-     // ======================= Practice Questions – Non-Functional =======================
-
-     @Then("User should see {string} link of practice question page")
-     public void user_should_see_link_of_practice_question_page(String topicName) {
-
-         switch (topicName) {
-             case "Search the array":
-                 assertTrue(pqPage.isSearchTheArrayDisplayed());
-                 break;
-             case "Max Consecutive Ones":
-                 assertTrue(pqPage.isMaxConsecutiveOnesDisplayed());
-                 break;
-             case "Find Numbers with Even Number of Digits":
-                 assertTrue(pqPage.isFindNumbersWithEvenNumberOfDigitsDisplayed());
-                 break;
-             case "Squares of a sorted Array":
-                 assertTrue(pqPage.isSquaresOfSortedArrayDisplayed());
-                 break;
-             default:
-                 throw new RuntimeException("Topic not found: " + topicName);
-         }
-     }
-
-     // ======================= Practice Questions – Functional =======================
-
-     @When("User clicks {string} link")
-     public void user_clicks_practice_question_link(String topicName) {
-
-         switch (topicName) {
-             case "Search the array":
-                 pqPage.clickSearchTheArray();
-                 break;
-             case "Max Consecutive Ones":
-                 pqPage.clickMaxConsecutiveOnes();
-                 break;
-             case "Find Numbers with Even Number of Digits":
-                 pqPage.clickFindNumbersWithEvenNumberOfDigits();
-                 break;
-             case "Squares of a sorted Array":
-                 pqPage.clickSquaresOfSortedArray();
-                 break;
-             default:
-                 throw new RuntimeException("Topic not found: " + topicName);
-         }
-     }
-
-     @Then("User should see Question, run button and submit button")
-     public void user_should_see_question_run_and_submit_buttons() {
-         assertTrue(pqPage.isQuestionTextVisible());
-         assertTrue(pqPage.isRunButtonVisible());
-         assertTrue(pqPage.isSubmitButtonVisible());
-     }
-
-     @Given("User is on the practice question editor of {string}")
-     public void user_is_on_practice_question_editor(String questionName) {
-         user_clicks_practice_question_link(questionName);
-         assertTrue(pqPage.isEditorVisible(),
-                 "Editor not visible for " + questionName);
-     }
-
-     @When("User write the {string} python code in Editor and Click the Run button")
-     public void user_write_code_and_click_run(String codeType) {
-
-         switch (codeType.toLowerCase()) {
-             case "valid":
-                 pqPage.enterCode(pqPage.getValidCode());
-                 break;
-             case "invalid":
-                 pqPage.enterCode(pqPage.getInvalidCode());
-                 break;
-             case "empty code":
-                 pqPage.clearEditor();
-                 break;
-             default:
-                 throw new RuntimeException("Invalid code type: " + codeType);
-         }
-         pqPage.clickRunButton();
-     }
-
-     @Then("User should able to see {string}")
-     public void user_should_able_to_see(String expectedResult) {
-
-         switch (expectedResult.toLowerCase()) {
-             case "output displayed":
-                 assertTrue(pqPage.isOutputVisible());
-                 break;
-             case "error message":
-                 assertTrue(pqPage.isErrorMessageVisible());
-                 break;
-             case "alert with error message":
-                 assertTrue(pqPage.isAlertPresent());
-                 break;
-             default:
-                 throw new RuntimeException("Invalid expected result: " + expectedResult);
-         }
-     }
+    @When("User clicks Try Here button in the respective Array page")
+    public void userClicksTryHereButtonInTheRespectiveArrayPage() {
+        arrayPage.clickTryHereInArrayLinkPage();
+    }
+    /*
+     * @Then("User should be redirected to try Editor page") public void
+     * user_should_be_redirected_to_try_editor_page() { String currentURL =
+     * driver.getCurrentUrl(); Assert.assertTrue(currentURL.contains("tryEditor"));
+     * }
+     */
 }
