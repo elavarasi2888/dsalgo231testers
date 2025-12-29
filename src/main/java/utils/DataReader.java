5ae61b9b94c58d09146c87930f2840ca787fa911
showri <showrijb@gmail.com>
2025-12-29 12:51:12 -0500

    Added TryEditorPage functionality

---
+++ src/main/java/utils/DataReader.java
@@ -0,0 +1,70 @@
+package utils;
+
+import factory.LoggerFactory;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataReader {
+
+    private Workbook workbook;
+    private DataFormatter formatter = new DataFormatter();
+
+    public DataReader(String filename) {
+        try {
+            FileInputStream fis = new FileInputStream(filename);
+            workbook = new XSSFWorkbook(fis);
+        } catch (Exception e) {
+            LoggerFactory.getLogger().error("Error while reading data {} file - {}", filename, e.getMessage());
+        }
+    }
+
+    public Map<String, String> getDataByScenarioName(String sheetName, String scenarioName) {
+        Sheet sheet = workbook.getSheet(sheetName);
+        if (sheet == null) {
+            throw new IllegalArgumentException("Sheet not found: " + sheetName);
+        }
+        Row headerRow = sheet.getRow(0);
+        if (headerRow == null) {
+            throw new IllegalStateException("Header row is missing");
+        }
+        int scenarioColumnIndex = -1;
+
+        for (Cell cell : headerRow) {
+            if ("ScenarioName".equalsIgnoreCase(formatter.formatCellValue(cell))) {
+                scenarioColumnIndex = cell.getColumnIndex();
+                break;
+            }
+        }
+        if (scenarioColumnIndex == -1) {
+            throw new IllegalStateException("ScenarioName column not found");
+        }
+
+        for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+            Row row = sheet.getRow(i);
+            if (row == null) continue;
+            String scenarioCellValue = formatter.formatCellValue(row.getCell(scenarioColumnIndex)).trim();
+            if (scenarioCellValue.equalsIgnoreCase(scenarioName)) {
+                return extractRowData(headerRow, row);
+            }
+        }
+        throw new IllegalArgumentException("Scenario not found in Excel: " + scenarioName);
+    }
+
+    private Map<String, String> extractRowData(Row headerRow, Row dataRow) {
+        Map<String, String> data = new HashMap<>();
+        for (int i = 0; i < headerRow.getLastCellNum(); i++) {
+            String header = formatter.formatCellValue(headerRow.getCell(i)).trim();
+            String value = formatter.formatCellValue(dataRow.getCell(i)).trim();
+            if ("empty".equalsIgnoreCase(value)) {
+                value = "";
+            }
+            data.put(header, value);
+        }
+        return data;
+    }
+}
+
--- src/test/resources/features/tryEditor.feature
+++ src/test/resources/features/tryEditor.feature
@@ -1,22 +1,23 @@
+@TryEditor
 Feature: Try Editor test cases
 
   Background:
     Given User is on the tryEditor page
-    
+
 # Non-Functional test
 
   Scenario: Verify Run button is visible in the try Editor page
     Then User should see Run button in the try editor page
-    
-# Functional tests
+
+ #Functional tests
 
   Scenario Outline: Verify user receives correct result for python code in the try editor page
-    When User write the "<code>" in the try editor page
+    When User write the code for following "<scenario>" in the try editor page
     When User clicks the Run button
-    Then User see appropriate "<result>"
+    Then User see appropriate result
 
     Examples:
-      | code                            | result |
-      | 'print(invalid code)'           | alert  |
-      | 'print("sum of 10 and 20: 30")' | output |
-      |                                 | alert  |
+      | scenario     |
+      | Valid Code   |
+      | Invalid Code |
+      | No Code      |
