06bed8c6aee8a8cf6155a5c588796ab3c71f6614
elavarasi2888 <elavarasi2888@gmail.com>
2025-12-30 22:11:38 -0500

    adding pom.xml and runner.java

--- src/test/java/testRunner/Runner.java
+++ src/test/java/testRunner/Runner.java
@@ -1,40 +1,33 @@
 package testRunner;
-
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
-
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import utils.ConfigReader;
 import java.io.IOException;
-
-
-@CucumberOptions(features = "src/test/resources/features", 
+@CucumberOptions(features = "src/test/resources/features",
 		glue = { "hooks", "stepDefinitions" },
 		// tags = "@DsAlgoPortal or @HomePage or @HomePageSignIn or @Queue or @Tree or
 		// @TryEditor",
 		// tags="@Register",
 		// tags="@Array",
-		tags = "@DsAlgoPortal", 
+		tags = "@DsAlgoPortal",
 		plugin = { "pretty", "html:cucumber-reports.html",
 				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, 
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" },
 		dryRun = false, monochrome = true)
-
 public class Runner extends AbstractTestNGCucumberTests {
-
 	@Override
 	@DataProvider(parallel = false)
 	public Object[][] scenarios() {
 		return super.scenarios();
 	}
-
 	@BeforeClass
 	@Parameters({ "browserType" })
 	public void beforeClass(@Optional String browser) throws IOException {
 		ConfigReader.setBrowserType(browser);
 	}
-	 
+	
 }
