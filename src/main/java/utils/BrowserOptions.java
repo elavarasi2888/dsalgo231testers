9910b70eaf06038235f82ea6c124dd1a01d55872
showri <showrijb@gmail.com>
2026-01-04 21:05:07 -0500

    updated testng.xml

--- src/main/java/utils/BrowserOptions.java
+++ src/main/java/utils/BrowserOptions.java
@@ -41,7 +41,7 @@ public class BrowserOptions {
         options.addArguments("start-maximized");
         //options.addArguments("--incognito");
         options.addArguments("--headless=new");
-       
+        options.addArguments("disable-gpu");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--remote-debugging-port=9222");
